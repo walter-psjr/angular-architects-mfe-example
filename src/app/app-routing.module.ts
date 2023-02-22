2b0ef9229a294f802a69aef86a9f4a8100feaf76
Walter Souza <walter.psjr@gmail.com>
2023-02-22 19:25:11 -0300

    Dynamic implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'membership',
+    loadChildren: () => loadRemoteModule({
+      type: 'manifest',
+      remoteName: 'membership',
+      exposedModule: './Module'
+    })
+    .then(m => m.MembershipModule)
+  },
+  {
+    path: 'treasury',
+    loadChildren: () => loadRemoteModule({
+      type: 'manifest',
+      remoteName: 'treasury',
+      exposedModule: './Module'
+    })
+    .then(m => m.TreasuryModule)
+  },
+  {
+    path: '',
+    redirectTo: 'membership',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, '/assets/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -10,7 +17,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (createTranslateLoader),
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
